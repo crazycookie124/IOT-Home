64093ddd7f00d6c17b4afc037aaef1f5ecd939ce
crazycookie124 <crazycookie124.320@gmail.com>
2025-03-12 01:50:34 +0000

    Made script work in level added fan

--- IOT TinyHouse/Assets/Scripts/Light_System.cs
+++ IOT TinyHouse/Assets/Scripts/Light_System.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class Light_System : MonoBehaviour
 {
-    private int light_amount = 1;
-    [SerializeField] private List<GameObject> lightObjects = new List<GameObject>(); // Store GameObjects
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Resize the list based on light_amount
-        while (lightObjects.Count < light_amount)
-        {
-            lightObjects.Add(null);
-        }
-        while (lightObjects.Count > light_amount)
-        {
-            lightObjects.RemoveAt(lightObjects.Count - 1);
-        }
-
-    }
-
-    // Update is called once per frame
+    public List<Light> lightSources = new List<Light>(); // List to hold multiple lights
+    public Material material;
+    public KeyCode tempKey = KeyCode.E;
     void Update()
     {
-        
+        if (Input.GetKeyDown(tempKey))
+        {
+            ToggleLights();
+        }
+    }
+
+    void ToggleLights()
+    {
+        bool anyLightOn = false;
+        foreach (Light light in lightSources)
+        {
+            if (light != null)
+            {
+                light.enabled = !light.enabled;
+                if (light.enabled) anyLightOn = true;
+            }
+           
+        }
+        if (anyLightOn)
+        {
+            
+            material.EnableKeyword("_EMISSION");
+        }
+        else
+        {
+            material.DisableKeyword("_EMISSION");
+
+        }
     }
 }
